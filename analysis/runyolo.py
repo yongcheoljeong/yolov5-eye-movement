8961e9a7a795893fa0d24dd688062f34e9c90ca1
jyc1306 <jyc1306@gmail.com>
2021-09-13 21:03:48 +0900

    path 경로 버그 수정

--- analysis/runyolo.py
+++ analysis/runyolo.py
@@ -1,7 +1,7 @@
 import os 
 
-def runyolo(video_name, exp_train, save_name):
-    detect_cmd = f'python detect.py --weights runs/train/{exp_train}/weights/last.pt --conf 0.5 --max-det=2 --save-txt --name "{save_name}" --line-thickness=1 --source ../videos/"{video_name}"'
+def runyolo(video_repo_path, video_name, exp_train, save_name):
+    detect_cmd = f'python detect.py --weights runs/train/{exp_train}/weights/last.pt --conf 0.5 --max-det=2 --save-txt --name "{save_name}" --line-thickness=1 --source {video_repo_path}/"{video_name}"'
     command = 'cd ../yolov5' + ' & ' + detect_cmd
 
     print(command)
