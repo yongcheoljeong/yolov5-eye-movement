7d60a88112a35255cbc538e849fe2f58d43ff422
jyc1306 <jyc1306@gmail.com>
2021-09-14 12:19:26 +0900

    fix bug .mp4 .MP4 issue

--- analysis/runyolo.py
+++ analysis/runyolo.py
@@ -1,7 +1,11 @@
 import os 
 
-def runyolo(video_repo_path, video_name, exp_train, save_name):
-    detect_cmd = f'python detect.py --weights runs/train/{exp_train}/weights/last.pt --conf 0.5 --max-det=2 --save-txt --name "{save_name}" --line-thickness=1 --source {video_repo_path}/"{video_name}"'
+def runyolo(video_repo_path, video_name, exp_train, save_name, nosave=True):
+    if nosave == True:
+        dosave = '--nosave'
+    else: 
+        dosave = ''
+    detect_cmd = f'python detect.py --weights runs/train/{exp_train}/weights/last.pt --conf 0.5 --max-det=2 --save-txt {dosave} --name "{save_name}" --line-thickness=1 --source {video_repo_path}/"{video_name}"'
     command = 'cd ../yolov5' + ' & ' + detect_cmd
 
     print(command)
